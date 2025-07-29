52cdef6a11f4d408874a2b52048ecf9faefd393b
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-29 17:24:53 +0200

    Adding missing element to public-api.ts

--- projects/profile-matcher-structure-lib/src/public-api.ts
+++ projects/profile-matcher-structure-lib/src/public-api.ts
@@ -9,4 +9,5 @@ export * from './lib/services/profile-matcher-root.service';
 export * from './lib/services/auth.service';
 export * from './lib/services/session.service';
 export * from './lib/services/project.service';
+export * from './lib/services/project-matcher-root.service';
 
