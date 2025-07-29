50bc06cb0eccbd4f2e6337d4e2cf0bc0d62de630
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-29 16:32:50 +0200

    Exporting methods

--- projects/profile-matcher-structure-lib/src/public-api.ts
+++ projects/profile-matcher-structure-lib/src/public-api.ts
@@ -1,10 +1,12 @@
 export * from './lib/models/profile';
 export * from './lib/models/profile-candidate-id';
 export * from './lib/models/profile-candidate-comment';
+export * from './lib/models/project';
 
 export * from './lib/services/profile.service';
 export * from './lib/services/profile-candidate-comment.service';
 export * from './lib/services/profile-matcher-root.service';
 export * from './lib/services/auth.service';
 export * from './lib/services/session.service';
+export * from './lib/services/project.service';
 
